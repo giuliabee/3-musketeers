1a3bb31e62e8add27b24d840754019b0aaf34086
giuliabee <giuliaboggiali@gmail.com>
2019-02-18 23:00:58 +0100

    written tests for starwars

--- starwars/src/index.js
+++ starwars/src/index.js
@@ -4,18 +4,18 @@ const starWarsNames = require('./starwars-names.json');
 const getRandomItem = uniqueRandomArray(starWarsNames);
 
 module.exports = {
-  all: starWarsNames,
-  random: random
+    all: starWarsNames,
+    random: random
 };
 
 function random(number) {
-  if (number === undefined) {
-    return getRandomItem();
-  } else {
-    const randomItems = [];
-    for (let i = 0; i < number; i++) {
-      randomItems.push(getRandomItem());
+    if (number === undefined) {
+        return getRandomItem();
+    } else {
+        const randomItems = [];
+        for (let i = 0; i < number; i++) {
+            randomItems.push(getRandomItem());
+        }
+        return randomItems;
     }
-    return randomItems;
-  }
 }
--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -2,30 +2,31 @@ const starWars = require('./index');
 const starWarsNames = require('./starwars-names.json');
 
 describe('starwars-names', () => {
-  describe('all', () => {
-    test('should be a fulfilled array', () => {
-      expect(starWars.all).toHaveLength(starWarsNames.length)
-    });
-    test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    describe('all', () => {
+        test('should be a fulfilled array', () => {
+            expect(starWars.all).toHaveLength(starWarsNames.length)
+        });
+        test('should be an array of strings', () => {
+            for (let key in starWars.all)
+                expect(typeof starWars.all[key]).toBe('string');
+        });
+
+        test('should contain `Luke Skywalker`', () => {
+            expect(starWars.all).toContain('Luke Skywalker');
+        });
+
+        test('should not contain `Ben Quadinaros`', () => {
+            expect(starWars.all).not.toContain('Ben Quadinaros');
+        });
     });
 
-    test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
+    describe('random', () => {
+        test('should return a random item from the starWars.all', () => {
+            expect(starWars.all).toContain(starWars.random());
+        });
 
-    test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+        test('should return an array of random items if passed a number', () => {
+            expect(starWars.all).toEqual(expect.arrayContaining(starWars.random(3)));
+        });
     });
-  });
-
-  describe('random', () => {
-    test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
-
-    test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
-  });
 });
