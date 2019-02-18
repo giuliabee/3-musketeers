71d649dc2af7f014e67047c91cc855f3c6ab9ce3
giuliabee <giuliaboggiali@gmail.com>
2019-02-18 18:14:58 +0100

    added comments in cash.js

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -6,13 +6,15 @@ const chalk = require('chalk');
 const ora = require('ora');
 const currencies = require('../lib/currencies.json');
 
-const {API} = require('./constants');
+const {API} = require('./<constants>');
 
 const cash = async command => {
+	//get parameters from command line
 	const {amount} = command;
 	const from = command.from.toUpperCase();
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
 
+	//print command status
 	console.log();
 	const loading = ora({
 		text: 'Converting...',
@@ -25,12 +27,14 @@ const cash = async command => {
 
 	loading.start();
 
+	//query API to get values of constants
 	await got(API, {
 		json: true
 	}).then(response => {
 		money.base = response.body.base;
 		money.rates = response.body.rates;
 
+		//check that all parameters are valid
 		to.forEach(item => {
 			if (currencies[item]) {
 				loading.succeed(`${chalk.green(money.convert(amount, {from, to: item}).toFixed(3))} ${`(${item})`} ${currencies[item]}`);
@@ -39,8 +43,10 @@ const cash = async command => {
 			}
 		});
 
+		//print conversion result
 		console.log(chalk.underline.gray(`\nConversion of ${chalk.bold(from)} ${chalk.bold(amount)}`));
 	}).catch(error => {
+		//check for errors
 		if (error.code === 'ENOTFOUND') {
 			loading.fail(chalk.red('Please check your internet connection!\n'));
 		} else {
